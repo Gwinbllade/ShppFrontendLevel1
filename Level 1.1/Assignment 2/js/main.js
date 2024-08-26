4c24fa0a5a33f8d9233ce5872c51e0e51bbb9b38
Ihor Bychkovskyi <igorbickovskij051@gmail.com>
2024-08-26 12:04:48 +0300

    comlpete all section

--- Level 1.1/Assignment 2/js/main.js
+++ Level 1.1/Assignment 2/js/main.js
@@ -1,3 +1,9 @@
-$(function(){
+$(function () {
+    window.onload = () => {
+        const grid = document.querySelector(".lates-news-grid-container")
+        const masonry = new Masonry(grid, {
+            gutter: 11
+        });
+    }
 
 });
