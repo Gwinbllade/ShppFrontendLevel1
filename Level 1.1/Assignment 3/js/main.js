b93da7112277504e52e24d1ba9a2354dad74b203
Ihor Bychkovskyi <igorbickovskij051@gmail.com>
2024-08-27 01:46:30 +0300

    add adaptive

---
+++ Level 1.1/Assignment 3/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    window.onload = () => {
+        const grid = document.querySelector(".lates-news-grid-container")
+        const masonry = new Masonry(grid, {
+            gutter: 11
+        });
+    }
+
+});
