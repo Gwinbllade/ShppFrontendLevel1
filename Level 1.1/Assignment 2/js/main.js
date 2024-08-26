e051ea3b8ed3d57e48f0aff97c6e922236a33e03
Ihor Bychkovskyi <igorbickovskij051@gmail.com>
2024-08-27 01:51:12 +0300

    refactoring repo

--- Level 1.1/Assignment 2/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    window.onload = () => {
-        const grid = document.querySelector(".lates-news-grid-container")
-        const masonry = new Masonry(grid, {
-            gutter: 11
-        });
-    }
-
-});
