e1ac98b3591a8c859a9a781f93e547331fa371d1
Ihor Bychkovskyi <igorbickovskij051@gmail.com>
2024-08-28 20:26:33 +0300

    add OG, title, icons. Make work carusel

--- Level 1.1/Assignment 3/js/main.js
+++ Level 1.1/Assignment 3/js/main.js
@@ -4,6 +4,21 @@ $(function () {
         const masonry = new Masonry(grid, {
             gutter: 11
         });
-    }
 
+        $(document).ready(function () {
+            $("#intro-carousel").owlCarousel({
+                items: 1,
+                dotsContainer: '#intro-carousel-dots',
+                loop: true
+            });
+        });
+
+        $(document).ready(function () {
+            $("#quote-carousel").owlCarousel({
+                items: 1,
+                dotsContainer: '#testimonial-carousel-dots',
+                loop: true
+            });
+        });
+    }
 });
